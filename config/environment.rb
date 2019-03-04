37e27319e85ab1a6bf93e0e2a260aca23823ee39
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-04 16:39:13 -0500

    update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -12,4 +12,11 @@ ActiveRecord::Base.establish_connection(
 
 # ActiveRecord::Base.logger = Logger.new(STDOUT)
 
+# connection_details = YAML::load(File.open('config/database.yml'))
+#
+# # DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
+# DB = ActiveRecord::Base.establish_connection(connection_details)
+
+
 require_all 'app'
+require 'json'
