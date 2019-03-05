b87bf608874b603e0fb9245be3185b394d4c6efd
seanlucius <seanlucius22@icloud.com>
2019-03-05 11:54:42 -0500

    updated ingredient and recipe models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,10 +2,28 @@ class Ingredient < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredient
 
-  def possible_cocktail
-    RecipeIngredient.where(ingredient_id: self)
+  # def possible_recipe_ingredients
+  #   RecipeIngredient.where(ingredient_id: self)
+  # end
+
+
+  def possible_cocktail_recipes
+    self.recipe_ingredients.map do |drink|
+      Recipe.find(drink.recipe_id)
+
+      # puts "#{Recipe.find(drink.recipe_id).name}"
+    end
   end
 
+  def possible_cocktails_two_ing(ing2)
+    self.possible_cocktail_recipes & ing2.possible_cocktail_recipes
+
+  end
+
+
+  # def possible_drinks
+  #   Recipe.where(id = self.recipe_ingredients.recipe_id)
+  # end
   # def possible_cocktail_with(liqour1, liqour2)
   #   RecipeIngredient.where(ingredient_id: Ingredient.find_by(name: liqour1)).each do |x|
   #   end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,13 +2,13 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredient
 
-  def ingredients
-    RecipeIngredient.where(recipe_id: self)
-  end
+  # def ingredients
+  #   RecipeIngredient.where(recipe_id: self)
+  # end
 
-  def cocktail_recipe
-    cocktail_ingredients = self.ingredients
-    cocktail_ingredients.each do |x|
+  def ingredients
+    # cocktail_ingredients = self.ingredients
+    self.recipe_ingredients.each do |x|
       puts "#{Ingredient.find(x.ingredient_id).name}: #{x.amount} cl"
     end
   end
