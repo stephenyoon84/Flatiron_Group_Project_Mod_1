43e1f87538b7f1f57035c18e51c5bfd905027d4f
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-05 11:57:39 -0500

    update main

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,29 @@
 ActiveRecord::Base.logger = nil
+
+module Screen
+   def self.clear
+       print "\ec\e[3J"
+   end
+
+   def self.next
+     user_input = nil
+     while user_input == nil
+       user_input = gets.chomp
+     end
+   end
+end
+
+class CocktailApp
+  def self.welcome
+
+    print "Welcome to Get Your Cocktail! You can search what kind of ingredients you need to make cocktail you want or you can get cocktail recipes with your own ingredients."
+    Screen.next
+    # Screen.clear
+  end
+
+  def self.invalid_option
+    print "Please select a valid option.\nInput: "
+    Screen.next
+    Screen.clear
+  end
+end
