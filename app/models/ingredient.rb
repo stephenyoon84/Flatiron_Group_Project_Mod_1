709f6a4302d64be2eef510ff7ac79376e1719a23
seanlucius <seanlucius22@icloud.com>
2019-03-05 14:16:48 -0500

    complete-conflicts

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,22 +2,13 @@ class Ingredient < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 
-<<<<<<< HEAD
+
   def print_possible_cocktails
     self.recipes.each { |drink| puts "#{drink.name}" }
-=======
-  def possible_cocktail_recipes
-    self.recipe_ingredients.map do |drink|
-      Recipe.find(drink.recipe_id)
-    end
->>>>>>> 295c363ea4b8383c45069de0f72b3718c6c48401
   end
 
   def possible_cocktails_two_ing(ing2)
     self.recipes & ing2.recipes
   end
-<<<<<<< HEAD
-  
-=======
->>>>>>> 295c363ea4b8383c45069de0f72b3718c6c48401
+
 end
