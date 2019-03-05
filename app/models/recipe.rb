42618eeb29bdecffe245bc5b4a14787f5bf2bbdd
seanlucius <seanlucius22@icloud.com>
2019-03-05 14:08:51 -0500

    added-print-methods-to-recipe-and-ingredient-models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,15 +1,9 @@
 class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
-  has_many :ingredients, through: :recipe_ingredient
+  has_many :ingredients, through: :recipe_ingredients
 
-  # def ingredients
-  #   RecipeIngredient.where(recipe_id: self)
-  # end
-
-  def ingredients
-    # cocktail_ingredients = self.ingredients
-    self.recipe_ingredients.each do |x|
-      puts "#{Ingredient.find(x.ingredient_id).name}: #{x.amount} cl"
-    end
+  def print_needed_ingredients
+    self.ingredients.each { |ingredient| puts "#{ingredient.name}" }
   end
+  
 end
